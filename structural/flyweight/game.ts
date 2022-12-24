fdbcfc6590cacb2b883bff84e1ba8034a17799e6
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-12-25 00:02:27 +0500

    Flyweight Pattern

---
+++ structural/flyweight/game.ts
@@ -0,0 +1,19 @@
+import { MoveableObject } from './moveable-object';
+import { StaticObject } from './static-object';
+
+export class Game {
+  constructor(private staticObjects: Array<StaticObject>, private movableObjects: Array<MoveableObject>) {
+    this.staticObjects = staticObjects;
+    this.movableObjects = movableObjects;
+  }
+
+  play(): void {
+    this.staticObjects.forEach((object) => console.log(object));
+    this.movableObjects.forEach((object) => {
+      console.log(object);
+      object.move();
+      object.jump();
+      console.log(object);
+    });
+  }
+};
---
+++ structural/flyweight/moveable-object.ts
@@ -0,0 +1,19 @@
+import { Point } from './point';
+import { StaticObject } from './static-object';
+
+export class MoveableObject {
+  constructor(public staticObject: StaticObject, public position : Point) {
+    this.staticObject = staticObject;
+    this.position = position;
+  }
+
+  move(): void {
+    this.position.x += 1;
+  }
+
+  async jump(): Promise<void> {
+    this.position.y += 5;
+    console.log(this);
+    this.position.y -= 5;
+  }
+};
